59a2cbee22364a0b5bcf76343487a7e5c0495756
Julessore21 <jules.sore13@gmail.com>
2022-07-06 17:50:20 +0200

    Test for dev

---
+++ js/json_input.js
@@ -0,0 +1,296 @@
+var data = `{
+	"data":[
+		{
+			"id":10,
+			"img_path":"picture/0.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Vue sur mer",
+			"vote_average":1.4,
+			"vote_count":1194
+		},
+		{
+			"id":11,
+			"img_path":"picture/9.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Allée artistique",
+			"vote_average":1,
+			"vote_count":118
+		},
+		{
+			"id":12,
+			"img_path":"picture/2.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Repas du midi",
+			"vote_average":6,
+			"vote_count":11846
+		},
+		{
+			"id":13,
+			"img_path":"picture/6.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Pains chauds",
+			"vote_average":9.8
+		},
+		{
+			"id":14,
+			"img_path":"picture/4.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Bougie",
+			"vote_average":7.1
+		},
+		{
+			"id":15,
+			"img_path":"picture/5.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Parapluie sous la pluie",
+			"vote_average":6.2
+		},
+		{
+			"id":16,
+			"img_path":"picture/1.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Travail en Binôme",
+			"vote_average":5
+		},
+		{
+			"id":17,
+			"img_path":"picture/7.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Hiver sur la banquise",
+			"vote_average":7
+		},
+		{
+			"id":18,
+			"img_path":"picture/8.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Châlet au milieu des sapins",
+			"vote_average":9.7,
+			"vote_count":447
+		},
+		{
+			"id":19,
+			"img_path":"picture/3.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Mariage",
+			"vote_average":1
+		},
+		{
+			"id":20,
+			"img_path":"picture/10.jpg",
+			"explanation":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+			"title":"Flou sur les immeubles",
+			"vote_average":9
+		}
+	],
+	"total":11
+}`;
+
+const obj = JSON.parse(data);
+
+/*Cards*/
+
+const obj1 = obj.data[0];
+change_couleur1(obj1.vote_average)
+document.getElementById('note1').innerHTML = obj1.vote_average;
+document.getElementById('title1').innerHTML = obj1.title;
+document.getElementById('text1').innerHTML = obj1.explanation;
+
+const obj2 = obj.data[1];
+change_couleur2(obj2.vote_average)
+document.getElementById('note2').innerHTML = obj2.vote_average;
+document.getElementById('title2').innerHTML = obj2.title;
+document.getElementById('text2').innerHTML = obj2.explanation;
+
+const obj3 = obj.data[2];
+change_couleur3(obj3.vote_average)
+document.getElementById('note3').innerHTML = obj3.vote_average;
+document.getElementById('title3').innerHTML = obj3.title;
+document.getElementById('text3').innerHTML = obj3.explanation;
+
+const obj4 = obj.data[3];
+change_couleur4(obj4.vote_average)
+document.getElementById('note4').innerHTML = obj4.vote_average;
+document.getElementById('title4').innerHTML = obj4.title;
+document.getElementById('text4').innerHTML = obj4.explanation;
+
+const obj5 = obj.data[4];
+change_couleur5(obj5.vote_average)
+document.getElementById('note5').innerHTML = obj5.vote_average;
+document.getElementById('title5').innerHTML = obj5.title;
+document.getElementById('text5').innerHTML = obj5.explanation;
+
+
+const obj6 = obj.data[5];
+change_couleur6(obj6.vote_average)
+document.getElementById('note6').innerHTML = obj6.vote_average;
+document.getElementById('title6').innerHTML = obj6.title;
+document.getElementById('text6').innerHTML = obj6.explanation;
+
+
+const obj7 = obj.data[6];
+change_couleur7(obj7.vote_average)
+document.getElementById('note7').innerHTML = obj7.vote_average;
+document.getElementById('title7').innerHTML = obj7.title;
+document.getElementById('text7').innerHTML = obj7.explanation;
+
+
+const obj8 = obj.data[7];
+change_couleur8(obj8.vote_average)
+document.getElementById('note8').innerHTML = obj8.vote_average;
+document.getElementById('title8').innerHTML = obj8.title;
+document.getElementById('text8').innerHTML = obj8.explanation;
+
+
+const obj9 = obj.data[8];
+change_couleur9(obj9.vote_average)
+document.getElementById('note9').innerHTML = obj9.vote_average;
+document.getElementById('title9').innerHTML = obj9.title;
+document.getElementById('text9').innerHTML = obj9.explanation;
+
+
+
+const obj10 = obj.data[9];
+change_couleur10(obj10.vote_average)
+document.getElementById('note10').innerHTML = obj10.vote_average;
+document.getElementById('title10').innerHTML = obj10.title;
+document.getElementById('text10').innerHTML = obj10.explanation;
+
+
+const obj11 = obj.data[10];
+change_couleur11(obj11.vote_average)
+document.getElementById('note11').innerHTML = obj11.vote_average;
+document.getElementById('title11').innerHTML = obj11.title;
+document.getElementById('text11').innerHTML = obj11.explanation;
+
+
+function change_couleur1(CA){
+    if(CA>=7){
+    document.getElementById('note1').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note1').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note1').style.color = "orange";
+	}
+}
+
+function change_couleur2(CA){
+    if(CA>=7){
+    document.getElementById('note2').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note2').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note2').style.color = "orange";
+	}
+}
+
+function change_couleur3(CA){
+    if(CA>=7){
+    document.getElementById('note3').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note3').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note3').style.color = "orange";
+	}
+}
+
+function change_couleur4(CA){
+    if(CA>=7){
+    document.getElementById('note4').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note4').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note4').style.color = "orange";
+	}
+}
+
+function change_couleur5(CA){
+    if(CA>=7.5){
+    document.getElementById('note5').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note5').style.color = "red";
+    }
+	if(CA<7.5 && CA>4){
+		document.getElementById('note5').style.color = "orange";
+	}
+}
+
+function change_couleur6(CA){
+    if(CA>=7){
+    document.getElementById('note6').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note6').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note6').style.color = "orange";
+	}
+}
+
+function change_couleur7(CA){
+    if(CA>=7){
+    document.getElementById('note7').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note7').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note7').style.color = "orange";
+	}
+}
+
+function change_couleur8(CA){
+    if(CA>=7){
+    document.getElementById('note8').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note8').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note8').style.color = "orange";
+	}
+}
+
+function change_couleur9(CA){
+    if(CA>=7){
+    document.getElementById('note9').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note9').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note9').style.color = "orange";
+	}
+}
+
+function change_couleur10(CA){
+    if(CA>=7){
+    document.getElementById('note10').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note10').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note10').style.color = "orange";
+	}
+}
+
+function change_couleur11(CA){
+    if(CA>=7){
+    document.getElementById('note11').style.color = "green";
+    }
+	if(CA<=4){
+    document.getElementById('note11').style.color = "red";
+    }
+	if(CA<7 && CA>4){
+		document.getElementById('note11').style.color = "orange";
+	}
+}
